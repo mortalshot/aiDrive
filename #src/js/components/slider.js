b037699f03182e408c28646ddb723afae9f15003
mortalshot <seregagl1996@gmail.com>
2020-12-01 13:28:36 +0300

    reviews section

---
+++ #src/js/components/slider.js
@@ -0,0 +1,7 @@
+$('.reviews__cards').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true,
+    adaptiveHeight: true,
+});
--- #src/js/index.js
+++ #src/js/index.js
@@ -1,3 +1,8 @@
+// libs
+@@include('libs/slick.min.js')
+
+// components
 @@include('components/webp.js')
 @@include('components/menu.js')
+@@include('components/slider.js')
 
